c4980bff0a4540df2ebbd503f18b1c4dbf7f7d9c
lynchlcl71 <lynchlcl@aol.com>
2016-12-10 19:47:09 -0500

    4.5 updated landing page with image carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
+  # def landing_page
+  #   @featured_product = Product.first
+  # end
+  
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
   
   def index
