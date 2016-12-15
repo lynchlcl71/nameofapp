b8fe6146be3ab30c053effbf4a74316a14db4544
lynchlcl71 <lynchlcl@aol.com>
2016-12-14 23:23:11 -0500

    5.1 created dynamic links to route users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   resources :products
+
+  get 'static_pages/index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
+
+  get 'static_pages/landing_page'
 
   # get 'static_pages/index' 
   root 'static_pages#landing_page'
