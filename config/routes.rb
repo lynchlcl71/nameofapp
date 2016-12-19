c331c0528875a9a49e1c0cf53df58eeb4811a13e
lynchlcl71 <lynchlcl@aol.com>
2016-12-18 23:39:12 -0500

    5.2 added search box and bootstrap sytled forms

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-  # get 'static_pages/index' 
   root 'static_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
