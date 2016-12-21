4aef5982b22685b053ea05271ebd1f2c12757712
lynchlcl71 <lynchlcl@aol.com>
2016-12-20 23:42:37 -0500

    5.3 created heroku and sendgrid emailers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,4 +9,15 @@ class StaticPagesController < ApplicationController
   
   def index
   end
-end
+
+  def contact
+  end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+
+  end
