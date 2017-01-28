7e7879afc7bd996ed155320ab932fee94ca017c4
lynchlcl71 <lynchlcl@aol.com>
2017-01-28 11:38:57 -0500

    6.5 setup payment forms with Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,11 @@
- 
+ if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_nzd1MzWN2u7Sg07gHnX4uwN4',
+    :secret_key => 'sk_test_mOCzQuDVL7FkjmCMwzUu78YN'
+  }
+end
