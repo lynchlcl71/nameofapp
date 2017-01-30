d13304347ae4571e764e15d5224edd474245b486
lynchlcl71 <lynchlcl@aol.com>
2017-01-29 22:46:27 -0500

    6.6 create payments for Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  post 'payments/create'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
