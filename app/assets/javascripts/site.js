e26c81ddc0b0f4510d376a18d95ca42bf56db97b
lynchlcl71 <lynchlcl@aol.com>
2017-02-09 18:39:35 -0500

    6.9 made revision to get zoom to work correctly

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,15 +1,39 @@
-$(document).on('turbolinks:load', function(){
-
-    //Raty
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
-    
-    // Initializing Elevate Zoom
-    $(".img-zoom").elevateZoom();
+};
+
+
+
+$(document).on('turbolinks:load ajaxSuccess', function(){refreshRating();
+    $('.img-zoom').elevateZoom({ zoomType: "lens", 
+      lensShape: "round", 
+    lensSize: 300, 
+    lensFadeIn: 500, 
+    lensFadeOut: 600 
   });
+});
+
+
+
+// $(document).on('turbolinks:load', function(){
+
+//     //Raty
+//     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+//     $('.rated').raty({ path: '/assets',
+//       readOnly: true,
+//       score: function() {
+//         return $(this).attr('data-score');
+//       }
+//     });
+    
+//     // Initializing Elevate Zoom
+//     $(".img-zoom").elevateZoom();
+//   });
  
+
