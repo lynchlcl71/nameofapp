f99b0001e904d90076d87d555ee8324477897d85
lynchlcl71 <lynchlcl@aol.com>
2017-02-28 18:39:04 -0500

    fixes for final achievement

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,17 @@ class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(:from => email,
         :to => 'lynch@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def payment_confirmation(user, product)
+    @user = user
+    @product = product
+    @appname = "Bike Berlin"
+    mail( :to => user.email, :subject "We appreciates your business #{@appname}.")
+  end
+
 end
