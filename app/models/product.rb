9299b16a5db851fabba2eb1919d8e7419a7d03bf
lynchlcl71 <lynchlcl@aol.com>
2017-03-02 17:34:17 -0500

    reverting to original redis code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,43 +1,12 @@
 class Product < ApplicationRecord
   has_many :orders
-  #added dependent: :destroy 
-  #to resolve issue in Dev.Env where 
-  #Destroy method failed on Products w/ Comments
-  has_many :comments, dependent: :destroy 
-  
-  validates :name, presence: true
-  #validates :description, 
-  validates :image_url, presence: true
-  #validates :color, 
-  validates :price, numericality: true
-
-
+  has_many :comments
   def highest_rating_comment
-    if comments.present?
-      comments.rating_desc.first
-    end
+    comments.rating_desc.first
   end
-
-  def lowest_rating_comment
-    if comments.present?
-      comments.rating_desc.last
-    end
-  end
-
   def average_rating
     comments.average(:rating).to_f
-  end 
-
-  def count_ratings
-    comments.count(:rating).to_int
   end
 
-  def views
-    $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
-  end
-
-  def viewed!
-    $redis.incr("product:#{id}") # this is equivalent to 'INC product:1'
-  end
-
+  validates :name, presence: true
 end
